e0c2d7179c38e234867d1eeb9cf97dfd9d7535f9
bennyjacob326 <bennyjacob326@gmail.com>
2022-09-13 15:18:54 -0400

    added search 2,3,4

--- HW1P1/search.py
+++ HW1P1/search.py
@@ -8,18 +8,14 @@ def search(n):
     s = state.create(n)  # creates a random state
     f = frontier.create(s)  # diff based on search way
     while not frontier.is_empty(f):
-        # count nums of removed
         s = frontier.remove(f)
         f[4] = f[4] + 1
         if state.is_target(s):
-            # counter of f[1], div by amount of times run
-
             frontier.adder(f[4], f[5], f[1])
             return [s, f[1]]
         ns = state.get_next(s)
         # print(ns)
         for i in ns:
-            # count nums of insert
             frontier.insert(f, i)
             f[5] = f[5] + 1
     return 0
@@ -31,4 +27,15 @@ for x in range(100):
 frontier.getAll()
 
 
+
+# search(2):
+# average depth: 1.74
+# average number pushed: 7.1
+# average number popped: 6.51
+#
+# search(3):
+# average depth: 6.04
+# average number pushed: 1177.26
+# average number popped: 678.3
+#search(4) wasn't possible
 
